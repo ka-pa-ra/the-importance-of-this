530b9cbcbc4ca1ee4a0493161d11d9cf93466c33
ka-pa-ra <capriolo63@gmail.com>
2020-06-17 14:01:09 +0000

    JQuery events

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//$('p').click(function(){
+ //   $('p').slideToggle('slow');
+//})
+
+//$('button').mouseenter(function(){
+   // $(this).removeClass('makeRed').addClass('makeBlue');
+//});
+//$('button').mouseleave(function(){
+  //  $(this).removeClass('makeBlue').addClass('makeRed');
+//});
+
+// Clicking on the box with the same class it will turn it red
+//$(document).ready(function(){
+  //$('.box').on("click", function(){
+      //var classNames = $(this).attr("class").split(" ");
+      //$("." + classNames[1]).css('background-color',  'red');
+  //});
+//});
+
+$(document).ready(function(){
+  $('.box').on("click", function(){
+      /**
+       * When we click on an element that has 
+       * a 'box' class, this event will be fired.
+       */
+     var classNames = $(this).attr("class").split(" ");
+     var boxClass = classNames[0];
+     var className = classNames[1];
+      if ($(this).css('background-color')== "rgb(255, 0, 0)"){
+      //if this object is already red turn it black
+      $("." + className).css('background-color',  '#000');
+      } else {
+          //Else turn all the elements  with a box black class
+          //and then change the color of all the element
+          //with the same class name as the clicked element 
+          //to red.
+          $("." + boxClass).css('background-color',  '#000');
+          $("." + className).css('background-color',  'red');
+      }
+  }) ;
+}) 
